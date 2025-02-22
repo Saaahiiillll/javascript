f576edd67b8ca365e609b7ae3d7b8020722e8eab
Saaahiiillll <116289048+Saaahiiillll@users.noreply.github.com>
2025-02-22 13:17:26 +0000

    Conversion of Datatype

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -22,3 +22,8 @@ console.log(booleanisLoggedIn); //true
 
 //" " => false   empty string
 //"Hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);  //33
+console.log(typeof stringNumber);  // string
