ebc64595fe572cdb61eab9f91b4664b844c7bff1
Saaahiiillll <116289048+Saaahiiillll@users.noreply.github.com>
2025-03-19 12:21:29 +0000

    callstack

---
+++ 03_basics/callStackLec25.js
@@ -0,0 +1,42 @@
+ /*Javascript execution context
+   {} --> Global execution context....  refers by this variable
+      function execution context
+      eval execution context
+
+    {}  memory creation phase
+        execution phase 
+
+        Now we gonna discuss how code execute in javascript
+        1. Global Execution or environment 
+          located by this
+        2. Memory Phase
+          val1 --> undefined
+          val2 --> undefined 
+          addnum --> definition 
+          result1 --> undefined
+          result2 --> undefined
+        
+        3. Execution Phase 
+        val1  <-- 10
+        val2  <-- 5
+        addNum -->   new variable environment + execution thread
+
+       . Memory Phase 
+        val1 --> undefined 
+        val2 --> undefined
+        total --> undefined
+       . execution Context
+        num1 --> 10
+        num2 --> 5
+        total --> 15 
+        
+
+         new variable environment + execution thread --> delete
+
+
+         call stack
+
+
+    SEE THE LECTURE FOR BETTER UNDERSTANDING 
+    
+ */
